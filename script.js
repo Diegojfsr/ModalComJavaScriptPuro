372cf4064028e6176977512fdfe3eb6f66dbb03d
DiegoJfsr <diegojfsr@gmail.com>
2024-12-07 12:30:09 -0300

    script

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ btn_modal.addEventListener('click', () => {
 cancel.addEventListener('click', () => {
     dialog.close();
 })
+
+
+// var btn_modal = document.querySelector("#btn_open_modal");
+// // var btn_modal = document.getElementById('btn_open_modal')
+
+// btn_modal.addEventListener('click', () => {
+//     dialog.showModal();
+// })
+
+
+// cancel.addEventListener('click', () => {
+//     dialog.close();
+// })
