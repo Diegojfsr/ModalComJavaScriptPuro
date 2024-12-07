7b48b88b8121fb3cc69d54e8fac9d308a35c4a22
DiegoJfsr <diegojfsr@gmail.com>
2024-12-07 13:07:15 -0300

    script

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-var btn_modal = document.querySelector("#btn_open_modal");
-// var btn_modal = document.getElementById('btn_open_modal')
+// var btn_modal = document.querySelector("#btn_open_modal");
+var btn_modal = document.getElementById('btn_open_modal');
 
 btn_modal.addEventListener(
     'click', () => {
